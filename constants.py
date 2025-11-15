91909b29bea4aebb661d8b3fb2a1ad0abd084f62
Frederic Borries <frederic@fabiv.pw>
2025-11-14 20:31:01 -0500

    remove unused vars

--- constants.py
+++ constants.py
@@ -4,11 +4,6 @@
 from astropy.constants import Constant
 from boinor.bodies import Body
 
-sun_mu = 139348062043.343  # km^3/s^2
-inital_x = 200 * 149597870.691  # km
-sec_per_day = 86400  # s/day
-day_per_yr = 365.25  # day/year
-
 
 class Sun:
     gravity = 139348062043.343  # km3/s2
