6937804c71aa04f67f67b5a1b322a56251ded202
Gabe <gnmcd13485@gmail.com>
2025-11-15 01:01:08 -0500

    move bodyDB

---
+++ bodyDB.py
@@ -0,0 +1,9 @@
+"""
+Holds the unpickled db
+"""
+with open("BodyDB.pickle", "rb") as file:
+    db: dict[int, tuple[tuple[float, float, float, float, float, float]]] = pickle.load(
+        file
+    )
+
+BODY_DB = db
